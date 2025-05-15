af092181a4b1436c705b1f57c98b08c2349ea596
Michał Skupiński <skupinski420@outlook.com>
2025-05-15 09:36:54 +0200

    first file heh

---
+++ movetracks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# make file executable
+# sudo chmod +x ~/movetracks.sh
+
+# make file available to run from any dir
+# sudo mv ~/movetracks.sh /usr/local/bin/
+# sudo chmod +x /usr/local/bin/movetracks
+
+# ${#array[@]} - # gives the number of elements in array
+# [@] means “all elements of the array.”
+
+HOME_DIR="/Users/skubi"
+DOWNLOADS_DIR="/Users/skubi/Downloads/"
+TRACKS_DIR="/Users/skubi/Downloads/tracks/"
+
+mkdir -p "$HOME_DIR/Downloads/tracks"
+
+# nullglob - if no files match the wildcard pattern, Bash expands it to an empty list
+shopt -s nullglob
+mp3_files=("$DOWNLOADS_DIR"*.mp3)
+shopt -u nullglob
+
+if [ ${#mp3_files[@]} -eq 0 ]; then
+  echo "no mp3 files"
+else 
+  mv "${mp3_files[@]}" "$TRACKS_DIR"
+  echo "${#mp3_files[@]} mp3 files moved"
+fi
