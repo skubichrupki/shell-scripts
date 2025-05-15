359fd03b1cef277b4f450e0de16c63565847c23e
Michał Skupiński <skupinski420@outlook.com>
2025-05-15 22:11:42 +0000

    useful tool as hell

--- movetracks.sh
+++ movetracks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# info:
 # make file available to run from any dir
 # sudo mv ~/movetracks.sh /usr/local/bin/
 # make file executable
@@ -8,17 +9,15 @@
 # ${#array[@]} - # gives the number of elements in array
 # [@] means “all elements of the array.”
 
-HOME_DIR="$HOME"
 DOWNLOADS_DIR="$HOME/Downloads/"
 TRACKS_DIR="$HOME/Music/_Serato_/Auto Import/"
 
-mkdir -p "$HOME_DIR/Downloads/tracks"
-
-# shopt enables, disables options for current shell session
+# shopt enables and disables options for current shell session
 # nullglob - if no files match the wildcard pattern, Bash expands it to an empty list
-shopt -s nullglob
-mp3_files=("$DOWNLOADS_DIR"*.mp3)
-shopt -u nullglob
+
+shopt -s nullglob # enable nulling empty assignments
+mp3_files=("$DOWNLOADS_DIR"*.mp3) # get all mp3 files
+shopt -u nullglob # disable nulling empty assignments
 
 if [ ${#mp3_files[@]} -eq 0 ]; then
   echo "no mp3 files"
